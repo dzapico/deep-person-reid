bca2e11cdecf197704a40b5f843a50d936c2106e
David Zapico García <dzapico@MacBook-Pro-de-David.local>
2023-04-03 10:55:02 +0200

    Changed market1501 url

--- torchreid/data/datasets/image/market1501.py
+++ torchreid/data/datasets/image/market1501.py
@@ -21,7 +21,7 @@ class Market1501(ImageDataset):
     """
     _junk_pids = [0, -1]
     dataset_dir = 'market1501'
-    dataset_url = 'http://188.138.127.15:81/Datasets/Market-1501-v15.09.15.zip'
+    dataset_url = 'https://www.googleapis.com/drive/v3/files/18oOMLTuAgdDbXglt8sOVGvYrzbJN_xuH?alt=media&key=AIzaSyBrvET80UzzR06JosxSacxcAjqWnjr-NkA'
 
     def __init__(self, root='', market1501_500k=False, **kwargs):
         self.root = osp.abspath(osp.expanduser(root))
